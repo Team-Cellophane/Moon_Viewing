b0d9240b90fd84a4cc84dde70f41f34eae876730
advance2112 <markdw1999@gmail.com>
2024-07-25 00:27:40 -0400

    Add semi-final 1st recipe, add rudimentary recipe entry. MVP

---
+++ Scripts/Recipe.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+@onready var _recipe_entry = $Recipe_Entry
+@onready var _correct_text = $CorrectText
+@onready var _incorrect_text = $IncorrectText
+
+func _on_button_pressed():
+	if (_recipe_entry.text == "w109,42"):
+		_correct_text.visible = true
+		_incorrect_text.visible = false
+	else:
+		_incorrect_text.visible = true
+		_correct_text.visible = false
