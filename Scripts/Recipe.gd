c5bb09e1d566cb73245eb14ec5ebb60d8a436295
Benjamin Marquez <bmarquez1997@gmail.com>
2024-07-27 04:12:35 -0400

    Adding buttons and qty text framework for recipe entry

--- Scripts/Recipe.gd
+++ Scripts/Recipe.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
-
-@onready var _recipe_entry = $Recipe_Entry
+@onready var _base_button = $"Base_Button"
+@onready var _ingredient_one = $"Ingredient-1_Node2D"
+@onready var _ingredient_two = $"Ingredient-2_Node2D"
+@onready var _ingredient_three = $"Ingredient-3_Node2D"
+@onready var _ingredient_four = $"Ingredient-4_Node2D"
+@onready var _ingredient_five = $"Ingredient-5_Node2D"
+@onready var _ingredient_six = $"Ingredient-6_Node2D"
 @onready var _correct_text = $CorrectText
 @onready var _incorrect_text = $IncorrectText
 
