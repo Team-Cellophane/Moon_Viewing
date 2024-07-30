8a4c0363258c327287e1d286199a90e71b587323
advance2112 <markdw1999@gmail.com>
2024-07-30 18:44:04 -0400

    Add font, fix copyright, add credits

--- Scripts/MainMenuController.gd
+++ Scripts/MainMenuController.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
 
-@onready var _title_moon = $Node/TitleMoon
-@onready var _title_viewing = $Node/TitleViewing
+@onready var _title = $Node/Title
 @onready var _title_animation_player = $Node/TitleAnimationPlayer
 @onready var _play_button = $PlayButton
 @onready var _background_animation_player = $Background/BackgroundAnimationPlayer
+@onready var _credits_animation_player = $Node2/CreditsAnimationPlayer
 @onready var _letter = $PlaintextLetter
 
 @onready var _continue_button = $ContinueButton
 
 func _ready():
-	if (Global.scene_number != 0):
-		_title_moon.visible = false
-		_title_viewing.visible = false
+	if (Global.scene_number != 0 && Global.get_current_recipe() != "credits"):
+		_title.visible = false
 		#play music/ sfx
 		_display_letter_or_continue()
+	elif (Global.get_current_recipe() == "credits"):
+		_do_end_credits()
 	else:
 		_title_animation_player.play("Fade_In")
 		_background_animation_player.play("Blur_Off")
@@ -39,3 +40,13 @@ func _on_play_button_pressed():
 	_letter.do_open()
 	_letter.set_target_position(Vector2(1920, 1080))
 	_continue_button.visible = true
+	
+func _do_end_credits():
+	_title_animation_player.play("Fade_In")
+	await _title_animation_player.animation_finished
+	_credits_animation_player.play("Credits")
+	await get_tree().create_timer(4).timeout
+	_title_animation_player.play("Fade_Out")
+	_background_animation_player.play("Blur_On")
+	await _credits_animation_player.animation_finished
+	_background_animation_player.play("Blur_Off")
